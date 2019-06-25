6d5efcdc2f19aa6badc82c07e02053088e959d49
hq7781 <hq7781@gmail.com>
2019-06-25 18:34:46 +0900

    init

---
+++ winkme/winkme/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  winkme
+//
+//  Created by 洪 権 on 2019/06/25.
+//  Copyright © 2019 洪 権. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
